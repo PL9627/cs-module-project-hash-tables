d3e37687edbb12cbf1f12e1624f6ebf0e75b73f7
Paul Lee <paul.lee0627@gmail.com>
2020-10-08 16:27:31 -0700

    solved histo but no hashmarks

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,22 @@
 # Your code here
+import string
 
+def list_from_file(filename):
+    myfile = open(filename, 'r')
+    data = myfile.read().split()
+    col = []
+    for word in data:
+        col.append(word)
+    return col
+
+def histo(col):
+    hist = {}
+    for word in col:
+        word = word.lower()
+        word = word.strip(string.punctuation + string.whitespace)
+        hist[word] = hist.get(word, 0) + 1
+    return hist
+
+col = list_from_file('robin.txt')
+colf = histo(col)
+print(colf)
